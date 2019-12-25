1e4dcba9f47be6d9cb8417cc0f165272fa618153
George Apetrei <george.apetrei@gmail.com>
2019-12-25 15:31:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ array.each_with_index do
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)# code an argument here
+elements.map{|element| element.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
