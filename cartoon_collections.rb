382767b4ee90814a9c00259741c952b28d051ef4
George Apetrei <george.apetrei@gmail.com>
2019-12-25 15:36:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(elements)# code an argument here
 elements.map{|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+
+
+
 end
 
 def find_the_cheese# code an argument here
