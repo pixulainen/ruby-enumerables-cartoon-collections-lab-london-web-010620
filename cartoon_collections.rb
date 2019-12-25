3c271511090121abce50800ea5a140dfb84a2941
George Apetrei <george.apetrei@gmail.com>
2019-12-25 16:17:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each{|item| if cheese_types.include?(item)
     return item  
+    nil
     end
-    return nil
    }
 end
